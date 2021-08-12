2dfe9a862e239826fa093499d8049a1b983fbf4a
Easy <easy-quest@mail.ru>
2021-08-12 07:57:12 +0000

    added test

--- sh.sh
+++ sh.sh
@@ -37,59 +37,77 @@ ZZZ
 
 # URLS
 
-cat > mysite/urls.py<<ZZZ
+# cat > mysite/urls.py<<ZZZ
 
-from django.contrib import admin
-from django.urls import path, include
+# from django.contrib import admin
+# from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-]
-ZZZ
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('pages.urls')),
+# ]
+# ZZZ
 
-cat > pages/urls.py <<ZZZ
-# pages/urls.py
-from django.urls import path
-from . import views
+# cat > pages/urls.py <<ZZZ
+# # pages/urls.py
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('about/', views.AboutPageView.as_view(), name='about'),
-]
-ZZZ
+# urlpatterns = [
+#     path('', views.HomePageView.as_view(), name='home'),
+#     path('about/', views.AboutPageView.as_view(), name='about'),
+# ]
+# ZZZ
 
-# Views
+# # Views
 
-cat > pages/views.py <<ZZZ
-# pages/views.py
-from django.views.generic import TemplateView
+# cat > pages/views.py <<ZZZ
+# # pages/views.py
+# from django.views.generic import TemplateView
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+# class HomePageView(TemplateView):
+#     template_name = 'home.html'
 
-class AboutPageView(TemplateView):
-    template_name = 'about.html'
-ZZZ
+# class AboutPageView(TemplateView):
+#     template_name = 'about.html'
+# ZZZ
 
-# HEROKU 
+# # HEROKU 
 
-cat > Procfile <<ZZZ
-web: gunicorn mb_project.wsgi --log-file -
-ZZZ
+# cat > Procfile <<ZZZ
+# web: gunicorn mb_project.wsgi --log-file -
+# ZZZ
 
-# TEST
+# # TEST
 
-cat > pages/tests.py <<ZZZ
-from django.test import SimpleTestCase
+# cat > pages/tests.py <<ZZZ
+# from django.test import SimpleTestCase
 
-class SimpleTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+# class SimpleTests(SimpleTestCase):
+#     def test_home_page_status_code(self):
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
 
 
-    def test_about_page_status_code(self):
-        response = self.client.get('/about/')
-        self.assertEqual(response.status_code, 200)
-ZZZ
+#     def test_about_page_status_code(self):
+#         response = self.client.get('/about/')
+#         self.assertEqual(response.status_code, 200)
+# ZZZ
+
+
+# # MODELS
+
+# cat > posts/models.py <<ZZZ
+# from django.db import models
+
+
+# class Post(models.Model):
+  
+#   text = models.TextField()
+# ZZZ
+
+
+# Tests 
+
+Раньше мы тестировали только статические страницы, поэтому мы использовали  SimpleTestCase(простой тест). Но теперь, когда наша домашняя страница работает с базой  данных, нам нужно использовать TestCase, которая позволит нам создать «тестовую» базу  данных, которую мы можем проверить. Другими словами, нам не нужно запускать тесты в  нашей реальной базе данных, но вместо этого можно создать отдельную тестовую базу  данных, заполнить ее образцами данных, а затем протестировать ее.
+Начнем с добавления образца записи в текстовое поле базы данных, а затем проверим,  правильно ли она хранится в базе данных. Важно, чтобы все наши методы тестирования  начинались с test_, чтобы Django знал что тестировать! Код будет выглядеть следующим  образом:
